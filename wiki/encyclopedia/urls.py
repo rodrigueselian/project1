55adbd136f0b54da60c625b9cb0c8c530e880074
Elian Rodrigues <rodrigues.elian@yahoo.com>
2020-08-04 14:46:16 -0300

    search bring up the first result

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.getpage, name="title")
+    path("wiki/<str:title>/", views.getpage, name="title"),
+    path("search", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import markdown2
 from . import util
-
+import logging
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -18,3 +18,19 @@ def getpage(request, title):
         })
     except TypeError:
         return render(request, "encyclopedia/error.html")
+
+def search(request):
+    query = request.POST["q"]
+    entries = util.list_entries()
+
+    for entry in entries:
+        if query.lower() in entry.lower():
+            page = util.get_entry(entry)
+            text = markdown2.markdown(page)
+            
+            return render(request, "encyclopedia/title.html", {
+                "page": entry,
+                "text": text
+            })
+        
+    return render(request, "encyclopedia/error.html")
